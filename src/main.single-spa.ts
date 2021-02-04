683b4135c05e12105e320f03a9ec158b12917f84
Anton <mun4kin@gmail.com>
2021-02-04 13:39:00 +0300

    fix

--- src/main.single-spa.ts
+++ src/main.single-spa.ts
@@ -2,56 +2,34 @@ import { enableProdMode, NgZone } from '@angular/core';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { Router } from '@angular/router';
-import { AppModule, DEFAULT_SWIPER_CONFIG } from './app/app.module';
+import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { singleSpaAngular } from 'single-spa-angular';
-import { APP_BASE_HREF } from '@angular/common';
-import { SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+import { getSingleSpaExtraProviders, singleSpaAngular } from 'single-spa-angular';
 // import { singleSpaPropsSubject } from './single-spa/single-spa-props';
 
-if (environment.production) {
-  enableProdMode();
-}
+const tegName = 'mainContent';
+environment.production && enableProdMode();
 
-function findTeg(tegName = ''):Promise<boolean> {
-  return new Promise((resolve) => {
-    let r;
-    r = document.getElementById(`${tegName}`);
-    r && resolve(true);
-    const i = setInterval(() => {
-      r = document.getElementById(`${tegName}`);
-
-      if (r) {
-        resolve(true);
-        clearInterval(i);
-      }
-    }, 500);
-
-  });
-}
+const findTeg = (tegName = ''):Promise<boolean> => new Promise((resolve) => {
+  document.getElementById(`${tegName}`) && resolve(true);
+  const i = setInterval(() => {
+    if (document.getElementById(`${tegName}`)) {
+      resolve(true);
+      clearInterval(i);
+    }
+  }, 50);
+});
 const lifecycles = singleSpaAngular({
-  bootstrapFunction: () => {
-    // singleSpaPropsSubject.next(singleSpaProps);
-    return platformBrowserDynamic([
-      {
-        provide: APP_BASE_HREF,
-        useValue: '/'
-      },
-      {
-        provide: SWIPER_CONFIG,
-        useValue: DEFAULT_SWIPER_CONFIG
-      }
-    ]).bootstrapModule(AppModule);
-  },
+  bootstrapFunction: () => platformBrowserDynamic(getSingleSpaExtraProviders()).bootstrapModule(AppModule),
   template: '<app-root />',
   Router,
   NgZone,
-  domElementGetter: () => document.getElementById('lego')
+  domElementGetter: () => document.getElementById(tegName)
 });
 
 export const bootstrap = [
   async() => {
-    findTeg('lego');
+    await findTeg(tegName);
     return true;
   },
   lifecycles.bootstrap
